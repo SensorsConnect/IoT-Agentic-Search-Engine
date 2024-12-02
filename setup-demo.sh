fd1d030d0ea83ff2e3d0936c18d3f86f16735385
elewah <abdelrahman.elewah@gmail.com>
2024-12-02 13:19:48 -0500

    prpareing shell script files

--- setup-demo.sh
+++ setup-demo.sh
@@ -5,6 +5,7 @@ while IFS= read -r package; do
     pip install "$package" || echo "Failed to install $package"
 done < requirements.txt
 
+pip install geopy;
 
 python -m spacy download en_core_web_lg 
 python -m spacy download en_core_web_sm 
